a6a31487d2fb2dc65a509d8d89d26820b47a644b
AlexPanich <nasada@yandex.ru>
2016-01-28 10:30:45 +0300

    добавлен мето prepareArray

--- App/DB.php
+++ App/DB.php
@@ -7,6 +7,21 @@ class DB
 {
     protected $dbh;
 
+    protected function prepareArray($sub) {
+        if (!$sub) {
+            return $sub;
+        }
+        $arr = [];
+        foreach ($sub as $key => $elem) {
+            if ($key[0] == ':') {
+                $arr[$key] = $elem;
+            } else {
+                $arr[':' . $key] = $elem;
+            }
+        }
+        return $arr;
+    }
+
     public function __construct()
     {
         $this->dbh = new \PDO('mysql:host=localhost;dbname=php2', 'root', '');
@@ -15,6 +30,7 @@ class DB
 
     public function execute($sql, $sub = [])
     {
+        $sub = $this->prepareArray($sub);
         $sth = $this->dbh->prepare($sql);
         $res = $sth->execute($sub);
         return $res;
@@ -22,6 +38,7 @@ class DB
 
     public function query($sql, $class, $sub = [])
     {
+        $sub = $this->prepareArray($sub);
         $sth = $this->dbh->prepare($sql);
         $res = $sth->execute($sub);
         if ( false != $res) {
