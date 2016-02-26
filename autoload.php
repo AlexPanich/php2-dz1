1e62a8c6884bd15d065e0fa45021f3ec44b68756
AlexPanich <nasada@yandex.ru>
2016-02-26 11:35:17 +0300

    доработана автозагрузка

--- autoload.php
+++ autoload.php
@@ -1,6 +1,12 @@
 <?php
 
-function __autoload($class)
-{
-    require_once __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-}
+spl_autoload_register(function ($class) {
+    $filename = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($filename)) {
+        require_once $filename;
+    } else {
+        return false;
+    }
+});
+
+require_once __DIR__ . '/vendor/autoload.php';
